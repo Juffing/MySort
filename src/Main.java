751c2aefd6e7da139883a1b0cef7862353ae4e79
Juffing <zhou_jun_feng@outlook.com>
2019-03-19 13:27:01 +0800

    完成了计数排序

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,75 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {6, 2, 4, 7, 9, 5, 1, 8, 3, 6, 6};
         System.out.println(Arrays.toString(arr));
+        arr = countingSort(arr);//计数排序
+//        mergeSort(arr);//归并排序
 //        shellSort(arr);//希尔排序
 //        heapSort(arr);//堆排序
         System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 计数排序方法，这个适用于正整数的数组，不适用负数和小数
+     * @param arr
+     */
+    public static int[] countingSort(int[] arr) {
+        int[] b = new int[arr.length];
+        int max = arr[0], min = arr[0];
+        for (int n : arr) {
+            if (n>max) {
+                max = n;
+            }
+            if (n<min) {
+                min = n;
+            }
+        }
+        int k = max - min +1;
+        int[] c = new int[k];
+        for (int i=0; i<arr.length; i++) {
+            c[arr[i]-min]++;
+        }
+        for (int i=1; i<k; i++) {
+            c[i] += c[i-1];
+        }
+        for (int i=arr.length-1; i>=0; i--) {
+            b[c[arr[i]-min]-1] = arr[i];
+            c[arr[i]-min]--;
+        }
+        return b;
+    }
+
+    /**
+     * 归并排序方法，调用mergeRecursionMethod方法
+     * @param arr
+     */
+    public static void mergeSort(int[] arr) {
+        int[] temp = new int[arr.length];
+        mergeRecursionMethod(arr, temp, 0, arr.length-1);
+    }
+
+    public static void mergeRecursionMethod(int[] arr, int[] temp, int start, int end) {
+        if (start<end) {
+            int mid = (start+end)/2;
+            mergeRecursionMethod(arr, temp, start, mid);
+            mergeRecursionMethod(arr, temp, mid+1, end);
+            int p1 = start, p2 = mid+1, k = start;
+            while (p1<=mid && p2<=end) {
+                if (arr[p1]>arr[p2]) {
+                    temp[k++] = arr[p2++];
+                } else {
+                    temp[k++] = arr[p1++];
+                }
+            }
+            while (p1<=mid) {
+                temp[k++] = arr[p1++];
+            }
+            while (p2<=end) {
+                temp[k++] = arr[p2++];
+            }
+            for (int i=start; i<=end; i++) {
+                arr[i] = temp[i];
+            }
+        }
     }
 
     /**
@@ -28,7 +94,7 @@ public class Main {
     }
 
     /**
-     * 堆排序方法
+     * 堆排序方法，调用changeAdjust方法
      * @param arr
      */
     public static void heapSort(int[] arr) {
