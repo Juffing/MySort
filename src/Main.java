5adbaa840b950d5664f4fe4ba6cc08c30efa3cad
Juffing <zhou_jun_feng@outlook.com>
2019-03-19 09:58:03 +0800

    完成了希尔算法

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,34 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {6, 2, 4, 7, 9, 5, 1, 8, 3, 6, 6};
-        shellSort(arr);
-//        heapSort(arr);
+        System.out.println(Arrays.toString(arr));
+//        shellSort(arr);//希尔排序
+//        heapSort(arr);//堆排序
         System.out.println(Arrays.toString(arr));
     }
 
+    /**
+     * 希尔排序方法
+     * @param arr
+     */
     public static void shellSort(int[] arr) {
         int length = arr.length;
+        int m, temp;
         for (int i=length/2; i>0; i=i/2) {
+            for (int n=i; n<length; n++) {
+                for (m=n; m>=i && arr[m]<arr[m-i]; m=m-i) {
+                    temp = arr[m];
+                    arr[m] = arr[m-i];
+                    arr[m-i] = temp;
+                }
+            }
         }
     }
 
+    /**
+     * 堆排序方法
+     * @param arr
+     */
     public static void heapSort(int[] arr) {
         int temp;
         for (int i=arr.length/2-1; i>=0; i--) {
